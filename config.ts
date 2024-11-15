86fe2b83c1cb5b06f7e9ffbe1ea414eca6b3b2fe
trangchongcheng <cheng.cit@gmail.com>
2024-11-15 14:35:12 +0700

    update token

--- config.ts
+++ config.ts
@@ -2,7 +2,7 @@ export const accounts = [
   {
     name: "NICK 1",
     refresh_token:
-      "AMf-vBy8ROnG9NKBypmYeRsT23tyq3UxRX4zwYKy_08WaqVRZCINMRToVyhs0ZK0kmXBDAsMMo5Cr-ws9Jdh150G81KNnLWFdOW3m0s_bwYnxDGC7cWSI9Ch3V3_y0FG1zdmlsOKwFzCaYyr9tl5uvNxtHid5vfv1yjJoJahGgvk-HCI87fxo2XwGg6k1XkEnlV5Gcdgj8e8-a1xKfRWotNnWbc9SnKcbWBdyI6VA_aXmrizwHddo03reQ216Lz7OdQAd2Hk4xe_bE5c4xj-ruhtMrb14fBkbxh-9f7NpLJM1TV6-NhEbBAUFYVr72BcV-Bj7_M_1MDoKnroeycTltCCr4vKDWXRHiOFkb-9l8DNnlDl987ftWohsD2AqQR6SfdD9hOS4d1W8SAK6IvXbFt1E0ukhRhlRe10_nVa2kLAS5Ol9Mu1z_emh4Ge96nBr6p4jSUKc23s",
+      "AMf-vBy8ROnG9NKBypmYeRsT23tyq3UxRX4zwYKy_08WaqVRZCINMRToVyhs0ZK0kmXBDAsMMo5Cr-ws9Jdh150G81KNnLWFdOW3m0s_bwYnxDGC7cWSI9Ch3V3_y0FG1zdmlsOKwFzCaYyr9tl5uvNxtHid5vfv1yjJoJahGgvk-HCI87fxo2XwGg6k1XkEnlV5Gcdgj8e8-a1xKfRWotNnWbc9SnKcbWBdyI6VA_aXmrizwHddo03reQ216Lz7OdQAd2Hk4xe_bE5c4xj-ruhtMrb14fBkbxh-9f7NpLJM1TV6-NhEbBAUFYVr72BcV-Bj7_M_1MDoKnroeycTltCCr4vKDWXRHiOFkb-9l8DNnlDl987ftWohsD2AqQR6SfdD9hOS4d1W8SAK6IvXbFt1E0ukhRhlRe10",
     proxy: "", //http://sp08-15050:MABBX@sp08-100.proxy.mkvn.net:15050
     access_token: "",
     privateKey: "",
